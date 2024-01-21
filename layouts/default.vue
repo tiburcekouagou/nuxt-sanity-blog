36447034e9a95bed3bd68114220a17d57d47519b
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-01-21 10:20:36 +0100

    set up project with tailwind

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <Link href="/">Nuxt + Sanity</Link>
+    </header>
+    <main>
+        <slot />
+    </main>
+    <footer>
+        <p>
+            <p>Icon at sanity</p>
+        </p>
+    </footer>
+</template>
